215e1279cdb2355d787405d1f68f6b7563cbd05f
Mary Diana Lucernas <110548247+MDiana17@users.noreply.github.com>
2023-02-02 22:14:20 +0800

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Enrollment System</h1>
+    <form @submit.prevent="submitEnrollment">
+      <label>
+        Name:
+        <input type="text" v-model="enrollment.name"/>
+      </label>
+      <label>
+        Email:
+        <input type="email" v-model="enrollment.email"/>
+      </label>
+      <label>
+        Course:
+        <select v-model="enrollment.course">
+          <option v-for="course in courses" :key="course">{{ course }}</option>
+        </select>
+      </label>
+      <button type="submit">Enroll</button>
+    </form>
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enrollment: {
+        name: '',
+        email: '',
+        course: ''
+      },
+      courses: ['Web Development', 'Cross-Platform Development Technology', 'Mobile Development'],
+      message: ''
+    }
+  },
+  methods: {
+    submitEnrollment() {
+      if (this.enrollment.name && this.enrollment.email && this.enrollment.course) {
+        this.message = 'Enrollment submitted successfully'
+        this.enrollment = { name: '', email: '', course: '' }
+      } else {
+        this.message = 'Please fill in all fields'
+      }
+    }
+  }
+}
+</script>
---
+++ router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _08f5bed1 = () => interopDefault(import('..\\pages\\bootstrap.min.js' /* webpackChunkName: "pages/bootstrap.min" */))
+const _6a5a0388 = () => interopDefault(import('..\\pages\\enrollment.vue' /* webpackChunkName: "pages/enrollment" */))
+const _4dbccefe = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/bootstrap.min",
+    component: _08f5bed1,
+    name: "bootstrap.min"
+  }, {
+    path: "/enrollment",
+    component: _6a5a0388,
+    name: "enrollment"
+  }, {
+    path: "/",
+    component: _4dbccefe,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
